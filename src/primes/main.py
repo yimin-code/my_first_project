93d42e59e4432eafd3807e33e6169fead3aae3c7
yimin-code <mikewym13456@gmail.com>
2026-07-30 22:53:27 -0700

    updated primes main

--- src/primes/main.py
+++ src/primes/main.py
@@ -1,4 +1,5 @@
 import math
+import time
 
 def main():
     print("Prime finding in Python.")
@@ -6,6 +7,40 @@ def main():
     n = 11
     prime_booleans = trivial_prime_finder(n)
     print(prime_booleans)
+    print(sieve_of_eratosthenes(n))
+
+    n = 2000000
+
+    # trivial timer
+    start = time.time()
+    trivial_prime_finder(n)
+    elapsed_trivial = time.time() - start
+    print(f"Trivial prime finder took {elapsed_trivial:.6f} seconds.")
+
+    # sieve timer
+    start = time.time()
+    sieve_of_eratosthenes(n)
+    elapsed_sieve = time.time() - start
+    print(f"Sieve of Eratosthenes took {elapsed_sieve:.6f} seconds.")
+
+    # ratio 
+    speedup = elapsed_trivial/elapsed_sieve
+    print(f"Speedup: {speedup:.2f}x faster")
+
+def list_primes(n: int) -> list[int]:
+    """
+    Returns a list of all primes up to and (possibly including) n.
+
+    Parameters:
+    -n: int
+
+    Output:
+    list[int]: list of all primes up to and including n.
+    """
+    if n < 0: 
+        raise ValueError("Error: negative integer given as input.")
+
+    
 
 def sieve_of_eratosthenes(n: int) -> list[bool]:
     """
@@ -36,7 +71,33 @@ def sieve_of_eratosthenes(n: int) -> list[bool]:
 
     return prime_booleans
 
+def cross_off_multiples(prime_booleans: list[bool], p: int) -> list[bool]:
+    """
+    Crosses off the multiples of p as composite numbers (i.e., not prime).
 
+    Parameters:
+    - prime_booleans: list[bool]
+    - p: int
+
+    Output: 
+    list[bool]: the element at index p will be True if p is prime and False if it is not
+    (i.e., prime_booleans[p] = True if p is prime)
+    """
+    if p < 2:
+        raise ValueError("Error: p must be at least 2.")
+
+    if len(prime_booleans) < 2:
+        raise ValueError("Error: prime_booleans must have length at least 2.")
+
+    n = len(prime_booleans) - 1 # the largest number we are checking for primality
+
+    for k in range(2*p, n+1, p): # start at 2*p and go up to n, with step size p
+        if k % p == 0:
+            prime_booleans[k] = False
+
+    # range over all indices of prime_booleans that are multiples of p, starting at 2*p and ending at the last index of prime_booleans, with step size p
+
+    return prime_booleans
 
 def trivial_prime_finder(n: int) -> list[bool]:
     """
